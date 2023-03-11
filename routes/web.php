6380ea93430bfca508abf66ea833a28d2df60408
ideacatlab <razvan@ideacat.ro>
2023-03-11 14:56:03 +0200

    include OpenAIController class in webroutes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OpenAIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 Route::get('/openai', [OpenAIController::class, 'sendRequest']);
