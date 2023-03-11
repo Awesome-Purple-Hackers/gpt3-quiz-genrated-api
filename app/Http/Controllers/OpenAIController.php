e211aeb4f78e3121f37dd1729862b52ef928958b
ideacatlab <razvan@ideacat.ro>
2023-03-11 17:18:24 +0200

    asdasda

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -22,22 +22,22 @@ class OpenAIController extends Controller
                 'Authorization' => 'Bearer ' . env('OPENAI_API_SECRET'),
             ],
         ]);
-
+    
         // Set up the request data.
         $requestData = [
             'model' => 'text-davinci-003',
-            'prompt' => 'I want you to create a quiz consisting of 1 question related to anything I tell you. The quiz should have this structure: [Question x][4 answers, numbered a,b,c,d][The letter of the correct answer] The answers should be as factual and accurate as possible, without ambiguity or falsehoods. Only return the quiz in this format, to be directly compatible with no-compressed Json Format: [ { \"question\": \"\", \"options\": [\"\"], \"correct\": \"\" } ] Highlight the code with backticks. The subject is math. Do not return anything else, just the json.\n\n',
+            'prompt' => 'I want you to create a quiz consisting of 1 question related to anything I tell you. The quiz should have this structure: [Question x][4 answers, numbered a,b,c,d][The letter of the correct answer] The answers should be as factual and accurate as possible, without ambiguity or falsehoods. Only return the quiz in this format, to be directly compatible with uncompressed Json Format: [ { \"question\": \"\", \"options\": [\"\"], \"correct\": \"\" } ] Highlight the code with backticks. The subject is math. Do not return anything else, just the json.\n\n',
             'temperature' => 0.7,
             'max_tokens' => 656,
             'top_p' => 1,
             'frequency_penalty' => 0,
             'presence_penalty' => 0,
         ];
-
+    
         // Send the request to the OpenAI API using the Guzzle client.
         $response = $client->post('completions', [
             'json' => $requestData,
-        ]);
+            ]);
 
         // Get the response body.
         $responseBody = json_decode($response->getBody(), true);
@@ -45,26 +45,9 @@ class OpenAIController extends Controller
         // Extract the generated quiz from the response body.
         $generatedQuiz = $responseBody['choices'][0]['text'];
 
-        // Parse the quiz into individual question, options, and correct answer variables.
-        preg_match('/\[ \{ \"question\": \"(.+)\", \"options\": \[(.+)\], \"correct\": \"(.+)\" \} \]/s', $generatedQuiz, $matches);
-
-        if (isset($matches[1], $matches[2], $matches[3])) {
-            $question = $matches[1];
-            $options = explode(', ', $matches[2]);
-            $correctAnswer = $matches[3];
-
-            // Compile the variables into a new JSON response.
-            $newJsonResponse = [
-                'question' => $question,
-                'options' => $options,
-                'correct_answer' => $correctAnswer,
-            ];
-
-            // Return the new JSON response.
-            return response()->json($newJsonResponse);
-        } else {
-            // Handle the case where the expected keys do not exist.
-            return response()->json(['error' => 'Unable to parse quiz.']);
-        }
+        // Return the generated quiz in JSON format.
+        return response()->json([
+            'quiz' => $generatedQuiz,
+        ]);
     }
 }
