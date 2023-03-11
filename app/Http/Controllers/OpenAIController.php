039ba8b09765e4f5283f22ff2eca889d7b65cfca
ideacatlab <razvan@ideacat.ro>
2023-03-11 20:04:24 +0200

    fix

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -110,40 +110,25 @@ public function sendRequest($numQuizzes = 1)
         // Get the $quizDomain parameter from the request query parameters.
         $quizDomain = $request->query('quiz_domain');
     
-        // Call the `sendRequest` method to generate the quizzes, passing the `$quizDomain` variable.
-        $quizzes = $this->sendRequest(1, $quizDomain);
+        // Retrieve the quizzes from the database.
+        $quizzes = Quiz::all();
     
-        // Create a new array to store the Quiz model IDs for the quizzes.
-        $quizIds = [];
-    
-        // Loop through the quizzes and create a new Quiz model for each one, then save it to the database if it doesn't already exist.
-        foreach ($quizzes as $quiz) {
-            $quizModel = Quiz::where('question', $quiz['question'])->first();
-    
-            if (!$quizModel) {
-                $quizModel = new Quiz();
-                $quizModel->question = $quiz['question'];
-                $quizModel->options = $quiz['options'];
-                $quizModel->correct_answer = $quiz['options'][array_search(true, array_column($quiz['options'], 'isCorrect'))]['answer'];
-                $quizModel->save();
-            }
-    
-            // Add the Quiz model ID to the array.
-            $quizIds[] = $quizModel->id;
+        // If the $quizDomain parameter is set, filter the quizzes by domain.
+        if ($quizDomain) {
+            $quizzes = $quizzes->filter(function ($quiz) use ($quizDomain) {
+                return stripos($quiz->question, $quizDomain) !== false;
+            });
         }
     
-        // Retrieve the Quiz models from the database.
-        $quizModels = Quiz::whereIn('id', $quizIds)->get();
-    
         // Transform the quizzes to the required format.
-        $formattedQuizzes = array_map(function ($quizModel) {
+        $formattedQuizzes = $quizzes->map(function ($quiz) {
             return [
-                'question' => $quizModel->question,
-                'options' => $quizModel->options,
+                'question' => $quiz->question,
+                'options' => $quiz->options,
             ];
-        }, $quizModels->toArray());
+        })->toArray();
     
         // Return the quizzes as a JSON response with a 200 status code.
         return response()->json($formattedQuizzes, 200);
-    }          
+    }      
 }
